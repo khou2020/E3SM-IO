cd3f3bcff4492549d1e3bf40eb76653219b1802b
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-03-01 00:37:40 -0600

    Update e3sm_io_hdf5.c

--- e3sm_io_hdf5.c
+++ e3sm_io_hdf5.c
@@ -757,7 +757,7 @@ int hdf5_put_varn_mpi (int vid,
             herr = H5Dwrite (did, mtype, msid, dsid, dxplid, bufp);
             CHECK_HERR
 */
-            if (i==0)
+            if (i<2)
             register_multidataset(bufp, did, dsid, msid, mtype);
 #endif
             twrite += MPI_Wtime () - te;

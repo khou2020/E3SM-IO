794107ecc609ad36feb288b1f3a7cce5381a81c4
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-03-01 02:16:02 -0600

    Update e3sm_io_hdf5.c

--- e3sm_io_hdf5.c
+++ e3sm_io_hdf5.c
@@ -203,6 +203,7 @@ int register_multidataset(void *buf, hid_t did, hid_t dsid, hid_t msid, hid_t mt
 
 int flush_multidatasets(){
     hsize_t dims[H5S_MAX_RANK], mdims[H5S_MAX_RANK];
+    int ndim;
     int i, j, rank;
     herr_t herr = 0;
 

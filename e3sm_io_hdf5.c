c30be0d1529b3f1a7d3570dcc4429ee20fff74b1
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-03-17 22:47:51 -0500

    Update e3sm_io_hdf5.c

--- e3sm_io_hdf5.c
+++ e3sm_io_hdf5.c
@@ -471,8 +471,6 @@ int hdf5_put_vara_mpi (
     hsize_t start[H5S_MAX_RANK], block[H5S_MAX_RANK];
     hsize_t dims[H5S_MAX_RANK];
 
-    return 0;
-
     did = f_dids[vid];
 
     dsid = H5Dget_space (did);
